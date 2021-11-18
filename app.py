0d87d32b87dc2eda50f6a0f04bd7839c1a6402c3
rodrigodadam <rodrigodadam@gmail.com>
2021-11-18 10:15:18 +0000

    Simple validation to do not kee the fields empty

--- app.py
+++ app.py
@@ -7,14 +7,19 @@ def index():
     return render_template('index.html')
 
 
-
+# Made the request form should get the data and render the success page.
+# TODO - Create a JS popup to give the feedback that the informations was received.
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
         client = request.form['client']
         rating = request.form['rating']
         comments = request.form['comments']
-        print(client, rating, comments)
+        # print(client, rating, comments)
+        # Simple validation, if the client do not insert the name He'll 
+        # receive a message to imput the name and index.html will be render again
+        if client == '':
+            return render_template('index.html', message='Please inser all required informations')
         return render_template('success.html')
 
 
