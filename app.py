06e99cf0442fd0aa3b138e6873b80dd02c35efa6
rodrigodadam <rodrigodadam@gmail.com>
2021-11-18 09:54:01 +0000

    Correction Type correction

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ def index():
     return render_template('index.html')
 
 
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        client = request.form['client']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        print(client, rating, comments)
+        return render_template('success.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
