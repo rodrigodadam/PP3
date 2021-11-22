61e68644c8239015b24fa4ef22b7b18bc1888bb9
rodrigodadam <rodrigodadam@gmail.com>
2021-11-22 14:12:52 +0000

    Organize models and routes

--- app.py
+++ app.py
@@ -8,31 +8,3 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG"),
     )
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Made the request form should get the data and render the success page.
-# # - Create a JS popup to give the feedback that the informations was received.
-# @app.route('/submit', methods=['POST'])
-# def submit():
-#     if request.method == 'POST':
-#         client = request.form['client']
-#         rating = request.form['rating']
-#         comments = request.form['comments']
-#         # print(client, rating, comments)
-#         # Simple validation, if the client do not insert the name He'll 
-#         # receive a message to imput the name and index.html will be render again
-#         if client == '':
-#             return render_template('index.html', message='Please insert all required informations')
-#         return render_template('success.html')
-
--- dgroup/routes.py
+++ dgroup/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from dgroup import app, db
 from dgroup .models import Feedback
 
 
 @app.route("/")
 def home():
+    return render_template("index.html")
+
+
+# Made the request form should get the data and render the success page.
+# - Create a JS popup to give the feedback that the informations was received.
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        fback = Feedback(
+            client = request.form["client"],
+            rating = request.form["rating"],
+            comments = request.form["comments"]
+        )
+        db.session.add(fback)
+        db.session.commit()
+        return render_template("success.html")
     return render_template("index.html")
