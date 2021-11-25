c159d5514cd44c07f447b3cfd6c273ec15084795
rodrigodadam <rodrigodadam@gmail.com>
2021-11-25 14:27:59 +0000

    Delete googleapi link

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import gspread
 from dgroup import app
-from google.oauth2.service_account import Credentials
-
 
 
 if __name__ == "__main__":
@@ -12,21 +10,3 @@ if __name__ == "__main__":
         debug=os.environ.get("DEBUG"),
     )
 
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('dgroup')
-
-feedback = SHEET.worksheet('feedback')
-
-data = feedback.get_all_values()
-
-print(data)
