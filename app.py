66db07ef69c7227d81eb86d5142b99385bc79fe7
rodrigodadam <rodrigodadam@gmail.com>
2021-11-25 13:24:00 +0000

    Correction README

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
+import gspread
 from dgroup import app
+from google.oauth2.service_account import Credentials
+
 
 
 if __name__ == "__main__":
@@ -8,3 +11,22 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG"),
     )
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('dgroup')
+
+feedback = SHEET.worksheet('feedback')
+
+data = feedback.get_all_values()
+
+print(data)
